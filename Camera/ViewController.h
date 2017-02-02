5fb19b93c8e6e4326109a4d0df3e5c9aa344a6e0
王慧勇 <weifeng19911024@163.com>
2017-02-02 10:20:48 +0800

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by wanghuiyong on 02/02/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
